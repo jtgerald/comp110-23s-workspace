bfadeb321b0e3da0c19b545cb41c07c534c421af
Josh Gerald <jgerald@unc.edu>
2023-01-30 12:44:56 -0500

    Finishing Touches

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -14,3 +14,31 @@ if len(character) != 1:
 
 print("Searching for " + character + " in " + word_choice)
 n: int = 0
+if (character == word_choice[0]):
+    print(str(character) + " found at index 0")
+    n = n + 1
+
+if (character == word_choice[1]):
+    print(str(character) + " found at index 1")
+    n = n + 1
+
+if (character == word_choice[2]):
+    print(str(character) + " found at index 2")
+    n = n + 1
+
+if (character == word_choice[3]):
+    print(str(character) + " found at index 3")
+    n = n + 1
+
+if (character == word_choice[4]):
+    print(str(character) + " found at index 4")
+    n = n + 1
+
+if n == 1:
+    print(str(1) + " instance of " + str(character) + " found in " + word_choice)
+
+if n > 1:
+    print(str(n) + " instances of " + str(character) + " found in " + word_choice)
+
+if n == 0:
+    print("No instances of " + str(character) + " found in " + word_choice)
