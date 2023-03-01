4cba059bae383b27f06aae6b1259285ef7a608eb
Josh Gerald <jgerald@unc.edu>
2023-03-01 08:25:20 -0500

    W Coding

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,37 @@
+"""List Utility Functions"""
+
+__author__ = 730475086
+
+def all(num: list[int], desire: int) -> bool:
+    ind: int = 0
+    if len(num) == 0:
+        return False
+    while len(num) > ind:
+        if num[ind] == desire:
+            ind += 1
+        else:
+            return False
+    return True
+
+def max(num2: list[int]) -> int:
+    ind: int = 0
+    if len(num2) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        x: int = num2[ind]
+        while len(num2) > ind + 1:
+            if x < num2[ind + 1]:
+                x = num2[ind + 1]
+            ind += 1
+        return x
+
+def is_equal(num3: list[int], num4: list[int]) -> bool:
+    i: int = 0
+    if len(num3) != len(num4):
+        return False
+    while i < len(num3):
+        if num3[i] == num4[i]:
+            i += 1
+        else:
+            return False
+    return True
